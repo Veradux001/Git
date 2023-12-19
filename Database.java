4b02b030e8b2a049a3c73a7fda481e5fd1113324
Dayal-Manbodh <dayalmanbodh@gmail.com>
2023-12-19 14:10:20 +0100

    database conectie

--- Database.java
+++ Database.java
@@ -1,14 +1,14 @@
 import java.sql.*;
 
-
 /**
  * Dit is een voorbeeld Java toepassing waarin je verbinding maakt met een
  * SQLServer database.
  */
 public class Database {
-    
 
-    private static String connectionUrl = "jdbc:sqlserver://localhost;databaseName=CodeCademyDD;integratedSecurity=false;encrypt=true;trustServerCertificate=true;";
+    // private static String connectionUrl =
+    // "jdbc:sqlserver://aei-sql2.avans.nl;1443;databaseName=CodeCademyDD;integratedSecurity=false;encrypt=true;trustServerCertificate=true;";
+    private static String connectionUrl = "jdbc:sqlserver://aei-sql2.avans.nl:1443;databaseName=CodeCademyDD;user=Steven;password=Striker@2024;encrypt=true;trustServerCertificate=true;";
     private static String driverName = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
     private static String username = "Steven";
     private static String password = "Striker@2024";
@@ -21,11 +21,11 @@ public class Database {
                 con = DriverManager.getConnection(connectionUrl, username, password);
             } catch (SQLException ex) {
                 // log an exception. fro example:
-                System.out.println("Failed to create the database connection."); 
+                System.out.println("Failed to create the database connection.");
             }
         } catch (ClassNotFoundException ex) {
             // log an exception. for example:
-            System.out.println("Driver not found."); 
+            System.out.println("Driver not found.");
         }
         return con;
     }
